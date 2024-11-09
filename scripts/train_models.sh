8293a13490d074a534eee55f7e49b24dc59a3b41
Nejc Hirci <nhirci@gmail.com>
2024-11-09 11:11:27 +0100

    Last commit before archive

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,8 +1,8 @@
 # Baseline Nerfacto model
-ns-train nerfacto --vis viewer+wandb --experiment-name redsea-nerfacto --data .\data\redsea\ colmap
+#ns-train nerfacto --vis viewer+wandb --experiment-name redsea-nerfacto --data .\data\redsea\ colmap
 
 # Nerfacto + Processed model
-ns-train nerfacto --vis viewer+wandb --experiment-name redsea-processed --data .\data\redsea-processed\ colmap
+#ns-train nerfacto --vis viewer+wandb --experiment-name redsea-processed --data .\data\redsea-processed\ colmap
 
 # Seathru model
-ns-train nerfacto --vis viewer+wandb --experiment-name redsea-seathrunerf --enable-seathru True --data .\data\redsea\ colmap
+ns-train seahtru-nerf --vis viewer+wandb --experiment-name redsea-seathrunerf --enable-seathru True --data .\data\redsea\ colmap
